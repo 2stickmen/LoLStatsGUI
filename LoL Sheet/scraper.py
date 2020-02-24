6777f521e959842f1b921897bff56689948b4d91
2stickmen <2stickmen@gmail.com>
2020-02-24 16:12:18 +0000

    Update scraper.py

--- LoL Sheet/scraper.py
+++ LoL Sheet/scraper.py
@@ -3,34 +3,31 @@ import scrapy
 from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
 from bs4 import BeautifulSoup
+import os
 
+golgg = 'https://gol.gg/champion/champion-stats/{}/season-S10/split-Spring/tournament-ALL/patch-ALL/role-ALL/league-1/'
 
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
 
-#class QuotesSpider(scrapy.Spider):
-#    name = "quotes"
-#
-#    def start_requests(self):
-#        urls = [
-#            'https://gol.gg/champion/champion-stats/92/season-S10/split-Spring/tournament-ALL/patch-ALL/role-ALL/league-1/',
-#        ]
-#        for url in urls:
-#            yield scrapy.Request(url=url, callback=self.parse)
-#
-#    def parse(self, response):
-#        page = response.url.split("/")[-2]
-#        filename = 'quotes-%s.html' % page
-#        with open(filename, 'wb') as f:
-#            f.write(response.body)
-#        self.log('Saved file %s' % filename)
-#        
-#configure_logging({'LOG_FORMAT': '%(levelname)s: %(message)s'})
-#runner = CrawlerRunner()
-#
-#d = runner.crawl(QuotesSpider)
-#d.addBoth(lambda _: reactor.stop())
-#reactor.run() # the script will block here until the crawling is finished\
-#
+    def start_requests(self):
+        urls = [golgg.format(i) for i in range(149)]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
 
+    def parse(self, response):
+        page = response.url.split("/")[-8]
+        filename = '-%s.html' % page
+        with open(os.getcwd() + '\\scraped\\' + filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
+        
+configure_logging({'LOG_FORMAT': '%(levelname)s: %(message)s'})
+runner = CrawlerRunner()
+
+d = runner.crawl(QuotesSpider)
+d.addBoth(lambda _: reactor.stop())
+reactor.run() # the script will block here until the crawling is finished\
 
 
 with open('thresh.html') as fp:
